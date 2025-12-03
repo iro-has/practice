f83d25665abe7c6a8aaf8b5e9d6f008520324cdc
iro-has <shunto2911@gmail.com>
2025-12-03 12:33:20 +0900

    クイズ機能の拡張（問題数を増やして、リファクタリングした）。

--- js-quiz/quiz.js
+++ js-quiz/quiz.js
@@ -1,21 +1,48 @@
-const question = '日本で一番高い山はどれですか？';
-const answers = [
-  '富士山',
-  'エベレスト',
-  'キリマンジャロ',
-  'マッキンリー'
+const quiz = [
+  {
+    question: '日本で一番高い山はどれですか？',
+    answers: [
+        '富士山',
+        'エベレスト',
+        'キリマンジャロ',
+        'マッキンリー'
+    ],
+    correct: '富士山'
+  },{
+    question: '日本で一番多い名字はどれですか？',
+    answers: [
+        '佐藤',
+        '鈴木',
+        '高橋',
+        '田中'
+    ],
+    correct: '佐藤'
+  },{
+    question: '日本の首都はどこですか？',
+    answers: [
+        '大阪',
+        '名古屋',
+        '東京',
+        '京都'
+    ],
+    correct: '東京'
+  }
 ];
-const correct = '富士山';
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
+
 const $button = document.getElementsByTagName('button');
 // クイズの問題文、選択肢を定義
 let buttonlength = $button.length;
 const setupQuiz = () => {
     // 問題文の表示
-    document.getElementById('js-question').textContent = question;
+    document.getElementById('js-question').textContent = quiz[quizIndex].question;
     // 選択肢の表示
     let buttonindex = 0;
     while(buttonindex < buttonlength) {
-        $button[buttonindex].textContent = answers[buttonindex];
+        $button[buttonindex].textContent = quiz[quizIndex].answers[buttonindex];
         buttonindex++;
     }
 };
@@ -23,10 +50,22 @@ setupQuiz();
 
 // ボタンにクリックイベントを設定
 const clickHandler = (e) => {
-    if(correct === e.target.textContent) {
+    if(quiz[quizIndex].correct === e.target.textContent) {
         window.alert('正解です！');
+        score++;
     } else {
         window.alert('不正解です！');
+    }
+    quizIndex++;
+    if(quizIndex < quizLength) {
+       // 問題数があれば実行する。
+        setupQuiz();
+    } else {
+         // 問題数がなければ終了する。
+        window.alert('終了！お疲れ様でした！あなたの正解数は' + score + '/' + quizLength + 'です！');
+        const win = window.open("https://example.com", "_blank");
+        win.close();  // OK
+
     }
 };
 
@@ -35,5 +74,5 @@ while(handlerIndex < buttonlength) {
     $button[handlerIndex].addEventListener('click', (e) => {
         clickHandler(e);
     });
-    handlerindex++;
+    handlerIndex++;
 }

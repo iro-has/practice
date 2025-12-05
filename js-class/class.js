8d6c0e12fd08a9e0b484808515304b3c7eda8085
iro-has <shunto2911@gmail.com>
2025-12-06 07:49:49 +0900

    クラスを作成しリファクタリングした。

--- js-class/class.js
+++ js-class/class.js
@@ -1,24 +1,33 @@
 (() => {
-    const $elm = document.querySelector('#js-accordion');
-    const $trigger = $elm.getElementsByTagName('a');
-    const $triggerLen = $trigger.length;
-    let index = 0;
-    while (index < $triggerLen) {
-        $trigger[index].addEventListener('click', (e) => clickHandler(e));
-        index++;
-    }
-
-    // クリックで実行される処理
-    const clickHandler = (e) => {
-        e.preventDefault();
-        
-        const $target = e.currentTarget;
-        const $content = $target.nextElementSibling;
-        if ($content.style.display === 'block') {
-            $content.style.display = 'none';
-        } else {
-            $content.style.display = 'block';
+    class Accordion {
+        constructor() {
+            const $elm = document.querySelector('#js-accordion');
+            const $trigger = $elm.getElementsByTagName('a');
+            const $triggerLen = $trigger.length;
+            let index = 0;
+            while (index < $triggerLen) {
+                $trigger[index].addEventListener('click', (e) => this.clickHandler(e));
+                index++;
+            }
         }
-    };
+        
+        // クリックで実行される処理
+        clickHandler = (e) => {
+            e.preventDefault();
+            
+            const $target = e.currentTarget;
+            const $content = $target.nextElementSibling;
+            if ($content.style.display === 'block') {
+                $content.style.display = 'none';
+            } else {
+                $content.style.display = 'block';
+            }
+        };
+
+    }
+    
+    const comeAccordion = new Accordion();
+ 
+
 
 })();

007d29d5e3e11c89ae4c8e5c77b73ea2ba6e343e
iro-has <shunto2911@gmail.com>
2025-12-08 09:14:46 +0900

    おみくじ機能の実装

---
+++ js-omikuji/omikuji.js
@@ -0,0 +1,20 @@
+//１．ランダムにおみくじ画像を表示する処理
+function getRandomOmikuji() {
+    const number = Math.floor (Math.random() * 7);
+    const omikujiImages = `./images/omikuji${number.toString()}.png`;
+    return omikujiImages;
+};
+
+//２．ボタンを押したときの処理
+function onClickButton() {
+    const timer = setInterval(() => {
+        document.querySelector('#js-result').setAttribute("src", getRandomOmikuji());
+    }, 100);
+    //３．数秒後に画像が止まる処理
+    setTimeout(() => {
+        clearInterval(timer);
+    }, 3000);
+};
+document.querySelector('#js-button').addEventListener('click', onClickButton);
+
+//３．数秒後に画像が止まる処理
